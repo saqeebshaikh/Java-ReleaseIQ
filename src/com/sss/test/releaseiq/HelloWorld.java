231163d28e6dbfeb8b82ae594ee48aefb4706393
Saqeeb Shaikh <saqeeb.shaikh136@gmail.com>
2021-09-30 12:24:48 +0530

    Update HelloWorld.java

--- src/com/sss/test/releaseiq/HelloWorld.java
+++ src/com/sss/test/releaseiq/HelloWorld.java
@@ -2,6 +2,6 @@ package com.sss.test.releaseiq;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World from Saqeeb!");
+        System.out.println("Hello World from Saqeeb!!");
     }
 }
