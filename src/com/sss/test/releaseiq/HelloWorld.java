23ad94c9d072bb40f26777e94531267db28d4fa1
abhinavshah2701 <abhinavk.shah@gmail.com>
2022-05-26 18:31:42 +0530

    246

--- src/com/sss/test/releaseiq/HelloWorld.java
+++ src/com/sss/test/releaseiq/HelloWorld.java
@@ -3,6 +3,6 @@ package com.sss.test.releaseiq;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World from Saqeeb!");
-        System.out.println("Test for RIQ build ID - QA245!");
+        System.out.println("Test for RIQ build ID - Prod246!");
     }
 }
